b6b0fc666abf4b0602d85a145be8ee5749fc26af
Roman Engeler <engeler.roman@gmail.com>
2022-12-29 14:52:37 +0100

    amplification. adaptive decomposition not with trace of previous questions

--- scripts/amplify_adaptive.py
+++ scripts/amplify_adaptive.py
@@ -1,32 +1,51 @@
 from ice.recipe import recipe
-from ice.recipes.primer.subquestions import ask_subquestions
 from ice.utils import map_async
 
 
 Question = str
 Answer = str
 Subs = list[tuple[Question, Answer]]
+Sups = list[Question]
 
-
-def make_evaluation_prompt(question: Question) -> str:
+# ================== Generate Prompts ==================
+# prompt to evaluate if to further decompose
+def make_evaluation_prompt(question: Question, sups: Sups) -> str:
     # get question
     # prompt to evaluate if to further decompose
-    return f"""Here is the question to be answered "{question}"
+    superquestions_text = render_superquestion(sups)
+    return f"""{superquestions_text}Here is the current subquestion to be answered
+    Q:"{question}"
 
-    Question: Do you need more information to answer this question? Say Yes or No.
+    Question: Do you need more information to answer this subquestion? Say Yes or No.
     Answer:"""
 
+# prompt to generate subquestions
+def make_subquestion_prompt(question: str) -> str:
+    return f"""Decompose the following question into 2-5 subquestions that would help you answer the question. Make the questions stand alone, so that they can be answered without the context of the original question.
 
-async def evaluate_decomposition(question: Question) -> float:
-    # get question
-    # evaluate if to further decompose
-    choice_probs, _ = await recipe.agent().classify(
-        prompt=make_evaluation_prompt(question),
-        choices=(" Yes", " No")
-    )
-    return choice_probs.get(" Yes", 0)
+Question: "{question}"
+Subquestions:
+-""".strip()
 
+# prompt to answer subquestion
+def make_qa_prompt(question: str, sups: Sups, subs: Subs) -> str:
+    background_text = render_background(subs)
+    superquestions_text = render_superquestion(sups)
 
+    return f"""{background_text}{superquestions_text}Answer the following question, using the background information above where helpful:
+
+    Question: "{question}"
+    Answer: "
+    """.strip()
+
+# prompt with superquestions
+def render_superquestion(sups: Sups) -> str:
+    if not sups:
+        return ""
+    sups_text = "\n".join(f"Q: {q}" for q in sups)
+    return f"Here are the relevant superquestions to answer:\n\n{sups_text}\n\n"
+
+# prompt with background information
 def render_background(subs: Subs) -> str:
     if not subs:
         return ""
@@ -34,31 +53,46 @@ def render_background(subs: Subs) -> str:
     return f"Here is relevant background information:\n\n{subs_text}\n\n"
 
 
-def make_qa_prompt(question: str, subs: Subs) -> str:
-    background_text = render_background(subs)
-    return f"""{background_text}Answer the following question, using the background information above where helpful:
+# ================== Generate Subquestions ==================
+# evaluate if to further decompose
+async def evaluate_decomposition(question: Question, sups: Sups) -> float:
+    # get question
+    # evaluate if to further decompose
+    choice_probs, _ = await recipe.agent().classify(
+        prompt=make_evaluation_prompt(question, sups),
+        choices=(" Yes", " No")
+    )
+    return choice_probs.get(" Yes", 0)
 
-Question: "{question}"
-Answer: "
-""".strip()
+# generate subquestions
+async def ask_subquestions(
+    question: str = "What is the effect of creatine on cognition?",
+):
+    prompt = make_subquestion_prompt(question)
+    subquestions_text = await recipe.agent().complete(prompt=prompt)
+    subquestions = [line.strip("- ") for line in subquestions_text.split("\n")]
+    return subquestions
 
-
-async def get_subs(question: str, depth: int) -> Subs:
+# ================== Main Routine ==================
+# receive question -> break into subquestions and answer them
+async def get_subs(question: str, sups: Sups, depth: int) -> Subs:
+    sups_copy = sups.copy() # make copy since passed by reference
+    sups_copy.append(question)
     subquestions = await ask_subquestions(question=question)
     subanswers = await map_async(
-        subquestions, lambda q: answer_by_amplification(question=q, depth=depth)
+        subquestions, lambda q: answer_by_amplification(question=q, sups=sups_copy, depth=depth)
     )
     return list(zip(subquestions, subanswers))
 
-
+# receive question -> break into subquestions and answer them -> answer question
 async def answer_by_amplification(
-    question: str = "What is the effect of creatine on cognition?", depth: int = 1
+    question: str = "What is the effect of creatine on cognition?", sups: Sups = [], depth: int = 1
 ):
     # adaptive decomposition
     #subs = await get_subs(question, depth - 1) if depth > 0 else []
-    decompose = round(await evaluate_decomposition(question))
-    subs = await get_subs(question, depth - 1) if (depth > 0 and decompose == 1) else []
-    prompt = make_qa_prompt(question, subs=subs)
+    decompose = round(await evaluate_decomposition(question, sups))
+    subs = await get_subs(question, sups, depth - 1) if (depth > 0 and decompose == 1) else []
+    prompt = make_qa_prompt(question, sups=sups, subs=subs)
     answer = await recipe.agent().complete(prompt=prompt, stop='"')
     return answer
 
